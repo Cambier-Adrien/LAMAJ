51f29656090ceb87f85aa9501177bff9705e76b1
Cambier Adrien <adriencambier04@gmail.com>
2023-12-15 16:40:06 +0100

    Github

---
+++ app/src/main/java/com/android/lamaj/SearchFrames.java
@@ -0,0 +1,18 @@
+package com.android.lamaj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+public class SearchFrames extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_frames);
+
+    }
+}
